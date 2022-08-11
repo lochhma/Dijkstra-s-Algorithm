b8e2218b3ebf79ae4a387c58cd043df5a351ee00
lochhma <lochhma@whs.school.nz>
2022-08-12 10:55:27 +1200

    weights began

---
+++ Weights.java
@@ -0,0 +1,18 @@
+
+/**
+ * Write a description of class Weights here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Weights
+{
+    int edge;//will have the value to point at one of the node
+    int weight;
+
+    public Weights(int edge,int weight)
+    {
+        this.edge=edge;
+        this.weight=weight;
+    }
+}
